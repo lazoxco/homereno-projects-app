60cdf38409f9a35986619d4e396da51d448fa84e
github username <github email address>
2021-03-09 02:00:53 -0500

    Building Project CRUD functionality.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+  
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    if params[:user_id] && @user = User.find_by_id(params[:user_id])
+      @projects = @user.projects
+    else
+      @error = "That user doesn't exist" if params[:user_id]
+      @projects = Project.all
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :content)
+  end
 end
