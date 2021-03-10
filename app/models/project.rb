5057c8c99a185565cd01827943c8bdb3fbd6143e
github username <github email address>
2021-03-10 01:02:56 -0500

    Added scope method to projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+
+  scope :most_comments, -> { joins(:comments).group('projects.id').order('count(projects.id) desc') }
 end
